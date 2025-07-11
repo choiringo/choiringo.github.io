aa2dd97be713ad4b564a956c9f6f55b360778aa9
choiringo <alsdk020477@naver.com>
2025-07-11 18:22:04 +0900

    Add files via upload

---
+++ 0711/script/index.js
@@ -0,0 +1,134 @@
+//타이틀,페이지네이션 전환용
+let slideNo = 0;
+
+function slide(no) {
+    slideNo += no;
+    if(slideNo > 1) slideNo = 0;
+    if(slideNo < 0) slideNo = 1;
+
+    //모든 타이틀 숨긴 후, 현재 번째 타이틀 보이기
+    $('.title').hide();
+    $('.title').eq(slideNo).show();    
+
+    //하단 진행율 표시 막대 크기 변경
+    $('.page').css({ 'opacity':'0.5', 'width':'50px' });
+    $('.page').eq(slideNo).css({ 'opacity':'1', 'width':'150px' });
+}
+
+//다음 버튼용 슬라이드
+function next() { 
+    $('#slideBox').stop().animate({'marginLeft':'-100%'},500,function(){
+        $('.slide:first').appendTo('#slideBox');
+        $('#slideBox').css('marginLeft','0');
+    });
+}
+
+//이전 버튼용 슬라이드
+function prev() {
+    $('.slide:last').prependTo('#slideBox');
+    $('#slideBox').css('marginLeft','-100%');
+    $('#slideBox').stop().animate({'marginLeft':'0'},500);
+}
+
+//자동 슬라이드
+let play;
+function playSlide() {   play = setInterval('next(); slide(1)', 3000);  }
+
+//자동 슬라이드 중지
+function stopSlide() {  clearInterval(play);  }
+
+
+$(function(){     //페이지가 열린 후,
+
+    wow = new WOW( {
+	boxClass:     'wow',      // default
+	animateClass: 'animated', // default
+	offset:       0,          // default
+	mobile:       true,       // default
+	live:         true        // default
+	} );
+
+	wow.init();	//WOW 초기화
+
+    playSlide();    //슬라이드 동작
+
+    $('.slideLeft').click(function(){     //이전 버튼 클릭
+        if( !$('#slideBox').is(':animated') ) {
+            prev();     //이전 슬라이드로 이동
+            slide(-1);
+        }
+    });
+
+    $('.slideRight').click(function(){    //다음 버튼 클릭
+        if( !$('#slideBox').is(':animated') ) {
+            next();     //다음 슬라이드로 이동
+            slide(1);
+        }
+    });
+
+    //좌우 버튼에 닿으면, 슬라이드 멈추기
+    $('.slideButton').mouseenter(function(){
+        stopSlide();
+    });
+
+    $('.slideButton').mouseleave(function(){
+        playSlide();
+    });
+
+
+    //슬라이드 중지, 재생 버튼 클릭할 때
+    $('.pause').click(function(){
+        stopSlide();
+        $('.control').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.play').click(function(){
+        playSlide();
+        $('.control').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+$('.hamburger').click(function(){  //햄버거메뉴(펼침메뉴)를 클릭했을 때
+    $(this).toggleClass('is-active');
+    $('#menu,#blackBox').fadeToggle(300);
+});          
+
+// 메인메뉴에 닿으면, 뒤 배경과 서브메뉴를 펼쳐보이기
+$('.main, #subBack').mouseenter(function(){
+    if( $(window).width() > 1000)
+        $('#subBack').stop().slideDown(300);
+});
+
+// 메인메뉴에 떠나면, 뒤 배경과 서브메뉴를 접어 숨기기
+$('.main, #subBack').mouseleave(function(){
+    $('#subBack, .sub').stop().slideUp(300);
+});
+
+//메인메뉴에 닿으면, 아이콘 변경하기
+$('.main').mouseenter(function(){
+    $('.icon').hide();
+    let no = $(this).index();  
+    $('.icon').eq(no).css('display','block');
+});
+
+
+$(window).resize(function(){
+    if($(window).width() > 1000) {
+        $('#menu').removeAttr('style');
+        $('.hamburger').removeClass('is-active');
+    }
+});
+
+});      //$(function() 끝
+
+// 화면 폭 900px 넘으면 메뉴, 배경박스, 검색박스 초기화 
+$(window).resize(function(){
+
+    if( $(window).width() > 900 ){
+        $('#menu').removeClass('menuOn');
+        $('#blackBox').hide();
+    }
+});
+
